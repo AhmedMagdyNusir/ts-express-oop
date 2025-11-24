71887ad44d98ee287fb80049d852b12312be9292
Ahmed Magdy <ahmedmagdynusir@gmail.com>
2025-11-24 17:50:09 +0200

    feat: add logger middleware and integrate it into app initialization

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application, Router } from "express";
+import { LoggerMiddleware } from "@/middlewares/logger.middleware";
 
 export class App {
   private app: Application;
 
-  constructor(private routes: { path: string; router: Router }[]) {
+  constructor(
+    private routes: { path: string; router: Router }[],
+    private loggerMiddleware: LoggerMiddleware,
+  ) {
     this.app = express();
     this.initializeMiddlewares();
     this.initializeRoutes();
@@ -16,6 +20,7 @@ export class App {
   }
 
   private initializeMiddlewares(): void {
+    this.app.use(this.loggerMiddleware.log);
     this.app.use(express.json());
   }
 
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import loadEnvironmentVariables from "@/utils/load-env-vars";
 
 loadEnvironmentVariables();
 
-import { App } from "./app";
+import { App } from "@/app";
 import { createUserRoutes } from "@/users/user.routes";
+import { LoggerMiddleware } from "@/middlewares/logger.middleware";
 import { PORT, ENVIRONMENT } from "@/utils/constants";
 
 if (!PORT) throw new Error("PORT is not defined in environment variables.");
 if (!ENVIRONMENT) throw new Error("ENVIRONMENT is not defined in environment variables.");
 
 const routes = [{ path: "/users", router: createUserRoutes() }];
+const loggerMiddleware = new LoggerMiddleware();
 
-const appInstance = new App(routes);
+const appInstance = new App(routes, loggerMiddleware);
 
 appInstance.start(Number(PORT), ENVIRONMENT);
