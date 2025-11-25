9fd2064a50e435232a2094e7043bc77780815014
Ahmed Magdy <ahmedmagdynusir@gmail.com>
2025-11-25 11:06:41 +0200

    feat: implement Config class for environment variable management and remove load-env-vars utility

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
-import loadEnvironmentVariables from "@/utils/load-env-vars";
-
-loadEnvironmentVariables();
-
 import { App } from "@/app";
+import { Config } from "@/utils/config";
 import { createUserRoutes } from "@/users/user.routes";
 import { LoggerMiddleware } from "@/middlewares/logger.middleware";
 import { ErrorHandlerMiddleware } from "@/middlewares/error-handler.middleware";
-import { PORT, ENVIRONMENT } from "@/utils/constants";
-import { isDevelopmentMode } from "@/utils/helpers";
 
-if (!PORT) throw new Error("PORT is not defined in environment variables.");
-if (!ENVIRONMENT) throw new Error("ENVIRONMENT is not defined in environment variables.");
+const envConfig = new Config();
+
+const PORT = envConfig.require("PORT");
+const ENVIRONMENT = envConfig.require("ENVIRONMENT");
 
 const routes = [{ path: "/users", router: createUserRoutes() }];
 const loggerMiddleware = new LoggerMiddleware();
-const errorHandlerMiddleware = new ErrorHandlerMiddleware(isDevelopmentMode);
+const errorHandlerMiddleware = new ErrorHandlerMiddleware(envConfig.isDevelopmentMode);
 
 const appInstance = new App(routes, loggerMiddleware, errorHandlerMiddleware);
 
